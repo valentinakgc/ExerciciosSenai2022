41e5222e0c53578f05ae81851e9d3c486ad93b6b
valentinakgc <99412232+valentinakgc@users.noreply.github.com>
2022-05-31 13:00:27 -0300

    Update script.js

--- localstorage/JS/script.js
+++ localstorage/JS/script.js
@@ -4,8 +4,35 @@ const addUsuario =() => {
     let nome = document.getElementById('nome').value
     let email = document.getElementById('email').value
     let senha = document.getElementById('senha').value
+    let divErro = document.querySelector('.div-erro')
 
+    if (nome == "" || email == "" || senha == ""){
+        divErro.style.display = 'block'
+    }else{
+        divErro.style.display = 'none'
+    }
+
+    if(localStorage.getItem('usuarios')){
+        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+    }
+   
     usuarios.push([nome, email, senha])
-    console.log('users', usuarios)
+    localStorage.setItem('usuarios', JSON.stringify(usuarios))
+    document.getElementById('nome').value = ""
+    document.getElementById('email').value = ""
+    document.getElementById('senha').value = ""
+}
+
+const listUsuarios = () => {
+    let tbody = document.getElementById('table-linha')
+    if(localStorage.getItem('usuarios') != null){
+        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+        usuarios.forEach (usuario => {
+            tbody.innerHTML += "<tr> <td>"+usuario[0]+"</td> <td>"+usuario[1]+"</td> </tr>"
+
+        });
+}else {
+    tbody.innerHTML += "<tr> <td>Vazio</td> <td>Vazio</td> </tr>"
+}
 
 }
