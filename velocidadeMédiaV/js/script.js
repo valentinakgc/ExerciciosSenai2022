07eccf610166b3a27889794932421a754dcfba73
valentinakgc <99412232+valentinakgc@users.noreply.github.com>
2022-09-18 01:40:55 -0300

    Update script.js

--- velocidadeMédiaV/js/script.js
+++ velocidadeMédiaV/js/script.js
@@ -9,6 +9,17 @@ function calcular() {
   let deslocamento = Number.parseFloat(document.getElementById("deslocamento").value);
   let tempo = Number.parseFloat(document.getElementById("tempo").value);
   let vm = deslocamento / tempo
-  document.getElementById("vm").value = vm
+  document.getElementById("vm").innerHTML = vm
+  respostas.push({dsl: deslocamento, tmp: tempo, vm: vm})
+  historico()
 }
 console.log(resultado);
+
+
+function historico() {
+  let fim = document.getElementById("historico")
+  fim.innerHTML = ''
+  for(let resposta of respostas) {
+    fim.innerHTML += `<li>Deslocamento: ${resposta.dsl} <br> Tempo: ${resposta.tmp} <br> Resposta: ${resposta.vm}</li>`
+  }
+}
